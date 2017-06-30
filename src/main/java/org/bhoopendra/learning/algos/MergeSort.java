9b9c04575a74b67ea248c6db94a93fb33fd91fe8
bhoopendra <bhoopendra.akgec@gmail.com>
2017-06-30 21:22:27 +0530

    Adding values

--- src/main/java/org/bhoopendra/learning/algos/MergeSort.java
+++ src/main/java/org/bhoopendra/learning/algos/MergeSort.java
@@ -3,7 +3,46 @@ package org.bhoopendra.learning.algos;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		
+		int arr[] = { 10, 2, 3, 6, 8, 9 };
+		new MergeSort().mergeSort(arr, 0, 5);
+		for(int item : arr){
+			System.out.println(item);
+		}
 	}
 
+	private void mergeSort(int arr[], int first, int last) {
+		int middle = first + (last - first) / 2;
+		if (first < last) {
+			mergeSort(arr, first, middle);
+			mergeSort(arr, middle + 1, last);
+			merge(arr, first, middle, last);
+		}
+
+	}
+
+	private void merge(int arr[], int first, int middle, int last) {
+		int helper[] = new int[arr.length];
+		for (int i = first; i <= last; i++) {
+			helper[i] = arr[i];
+		}
+		int i = first;
+		int j = middle + 1;
+		int k = first;
+
+		while (i <= middle && j <= last) {
+			if (helper[i] <= helper[j]) {
+				arr[k] = helper[i];
+				i++;
+			} else {
+				arr[k] = helper[j];
+				j++;
+			}
+			k++;
+		}
+		while (i <= middle) {
+			arr[k] = helper[i];
+			i++;
+			k++;
+		}
+	}
 }
