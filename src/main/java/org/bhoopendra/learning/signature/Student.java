9e5c385b019b877b327b6a5125ab8d9b2ad2a2c2
bhoopendra <bhoopendra.akgec@gmail.com>
2017-07-01 23:01:21 +0530

    committing some files

---
+++ src/main/java/org/bhoopendra/learning/signature/Student.java
@@ -0,0 +1,46 @@
+package org.bhoopendra.learning.signature;
+
+public class Student {
+	private String name;
+	private int rollNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollNumber() {
+		return rollNumber;
+	}
+	public void setRollNumber(int rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + rollNumber;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (rollNumber != other.rollNumber)
+			return false;
+		return true;
+	}
+	
+
+}
