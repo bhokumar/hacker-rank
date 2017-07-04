c2eb3d1f9274a34a8210ad45bbf3ecbb2291d24b
bhoopendra <bhoopendra.akgec@gmail.com>
2017-07-04 21:24:24 +0530

    Adding some files

---
+++ src/main/java/org/bhoopendra/learning/collection/CollectionCliet2.java
@@ -0,0 +1,31 @@
+package org.bhoopendra.learning.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionCliet2 {
+
+	public static void main(String[] args) {
+		List<Integer> inetgers = new ArrayList<>();
+		for (int i = -3; i < 3; i++) {
+			inetgers.add(i);
+		}
+
+		Iterator<Integer> iterator = inetgers.iterator();
+		while (iterator.hasNext()) {
+			Integer integer = iterator.next();
+			System.out.println(integer);
+		}
+		
+		for(int i=0;i<3;i++){
+			inetgers.remove(i);
+		}
+		
+		System.out.println("After removal");
+		for(int item : inetgers){
+			System.out.println(item);
+		}
+	}
+
+}
---
+++ src/main/java/org/bhoopendra/learning/inheritence/InheritanceClient.java
@@ -0,0 +1,24 @@
+package org.bhoopendra.learning.inheritence;
+
+public class InheritanceClient {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+interface A {
+	void show(Object object);
+}
+
+class B implements A {
+
+	@Override
+	public void show(Object i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+	
