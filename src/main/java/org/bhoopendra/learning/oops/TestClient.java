40d7a6ab3f363fda72af57b6ddc1ea874ce3f21c
bhokumar <bhoopendra.akgec@gmail.com>
2017-07-06 15:17:18 +0530

    Adding files

---
+++ src/main/java/org/bhoopendra/learning/oops/TestClient.java
@@ -0,0 +1,37 @@
+package org.bhoopendra.learning.oops;
+
+/**
+ * Created by bhokumar on 7/6/2017.
+ */
+public class TestClient {
+}
+
+interface  A{
+    void show();
+}
+
+interface  B{
+    void display();
+}
+
+interface C extends A,B{
+    void iterate();
+}
+
+class D implements C{
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void iterate() {
+
+    }
+}
