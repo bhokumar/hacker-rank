70085383dfa5b45fe8bd3260457dddbcde654c1d
bhoopendra <bhoopendra.akgec@gmail.com>
2017-07-10 07:59:15 +0530

    Adding files

--- src/main/java/org/bhoopendra/learning/thread/pnc/ProducerConsumerClientWithoutBlockingQueue1.java
+++ src/main/java/org/bhoopendra/learning/thread/pnc/ProducerConsumerClientWithoutBlockingQueue1.java
@@ -1,7 +1,81 @@
 package org.bhoopendra.learning.thread.pnc;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by BHOOPENDRA on 7/9/2017.
  */
 public class ProducerConsumerClientWithoutBlockingQueue1 {
+    public static void main(String[] args) {
+        final Queue<Integer> sharedQueue = new LinkedList<>();
+        Thread thread = new Thread(new Producer1(sharedQueue,4),"producer");
+        Thread thread1 = new Thread(new Consumer1(sharedQueue),"consumer");
+        thread.start();
+        thread1.start();
+
+    }
+
+}
+
+class Producer1 implements Runnable{
+    private Queue<Integer> sharedQueue;
+    private int maxSize;
+
+    public Producer1(final Queue<Integer> sharedQueue, final int maxSize) {
+        this.sharedQueue = sharedQueue;
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0;i<10;i++){
+            try {
+                produce(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void produce(final int item) throws InterruptedException {
+        synchronized (sharedQueue) {
+            while (sharedQueue.size() == maxSize) {
+                    sharedQueue.wait();
+                    Thread.sleep(10000);
+            }
+            sharedQueue.add(item);
+            System.out.println("produced item : "+item);
+            sharedQueue.notifyAll();
+        }
+    }
+}
+
+class Consumer1 implements Runnable{
+    private Queue<Integer> sharedQueue;
+
+    public Consumer1(Queue<Integer> sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    public void run() {
+        for (int i=0;i<10;i++){
+            try {
+                consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void consume() throws InterruptedException {
+        synchronized (sharedQueue){
+            while (sharedQueue.size()==0){
+                    System.out.println("consumer thread is waiting to item to be produced"+sharedQueue.size());
+                    sharedQueue.wait();
+            }
+            System.out.println("Element removed"+sharedQueue.remove());
+            sharedQueue.notifyAll();
+        }
+    }
 }
--- src/main/java/org/bhoopendra/learning/thread/pnc/SysnchronizeTest.java
+++ src/main/java/org/bhoopendra/learning/thread/pnc/SysnchronizeTest.java
@@ -4,4 +4,66 @@ package org.bhoopendra.learning.thread.pnc;
  * Created by BHOOPENDRA on 7/9/2017.
  */
 public class SysnchronizeTest {
+    public static void main(String[] args) {
+        Object object = new Object();
+        new Thread(new ProducerTest(object)).start();
+        new Thread(new ConsumerTest(object)).start();
+    }
+}
+
+class ProducerTest implements Runnable{
+
+    private Object object;
+
+    public ProducerTest(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+        try {
+            produce();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void produce() throws InterruptedException {
+        synchronized (object){
+            System.out.println(Thread.currentThread().getName()+"waiting");
+            object.wait();
+            System.out.println(Thread.currentThread().getName()+" going to sleep");
+            Thread.sleep(10000);
+            System.out.println("producer thread waked up");
+        }
+    }
+
+
+}
+
+class ConsumerTest implements Runnable{
+    private Object object;
+
+    public ConsumerTest(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public void run() {
+        try {
+            consume();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void consume() throws InterruptedException {
+
+        synchronized (object) {
+            System.out.println("consumer got the execution");
+            Thread.sleep(5000);
+            System.out.println("consumer has finioshed its execution");
+            object.notify();
+        }
+    }
 }
