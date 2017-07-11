f6c70c18089882256af69230c01122edc0c291de
bhokumar <bhoopendra.akgec@gmail.com>
2017-07-11 11:12:30 +0530

    Adding some code

--- src/main/java/org/bhoopendra/learning/enum1/EnumAbstractTest.java
+++ src/main/java/org/bhoopendra/learning/enum1/EnumAbstractTest.java
@@ -4,7 +4,7 @@ package org.bhoopendra.learning.enum1;
  * Created by bhokumar on 7/11/2017.
  */
 public enum EnumAbstractTest {
-    FIRST(1) {
+    FIRST(1,"first") {
         int i=100;
         void test(){
             System.out.println(i);
@@ -14,20 +14,21 @@ public enum EnumAbstractTest {
             test();
             System.out.println("first");
         }
-    },SECOND(2) {
+    },SECOND(2,"second") {
         @Override
         void calculateTest() {
             System.out.println();
         }
-    },THIRD(4) {
+    },THIRD(4,"third") {
         @Override
         void calculateTest() {
 
         }
     };
 
-    private EnumAbstractTest(final int i) {
-        System.out.println(i);
+    private EnumAbstractTest(final int i,final String name) {
+        System.out.print(i);
+        System.out.println(" name "+name);
     }
 
     abstract void calculateTest();
