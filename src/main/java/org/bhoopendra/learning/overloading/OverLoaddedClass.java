e8e6cd6906f089baaceb2337a341e98cf4e9dc01
bhokumar <bhoopendra.akgec@gmail.com>
2017-07-12 09:43:46 +0530

    Adding some files

---
+++ src/main/java/org/bhoopendra/learning/overloading/OverLoaddedClass.java
@@ -0,0 +1,13 @@
+package org.bhoopendra.learning.overloading;
+
+/**
+ * Created by bhokumar on 7/12/2017.
+ */
+public class OverLoaddedClass {
+    void method(int a){
+
+    }
+    int method(){
+        return 1;
+    }
+}
