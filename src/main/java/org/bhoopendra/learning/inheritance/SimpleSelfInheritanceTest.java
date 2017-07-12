696488cd5ee6d17c984a7146b29ccdd0c78047a2
bhokumar <bhoopendra.akgec@gmail.com>
2017-07-12 08:38:41 +0530

    Adding files

---
+++ src/main/java/org/bhoopendra/learning/inheritance/SimpleSelfInheritanceTest.java
@@ -0,0 +1,20 @@
+package org.bhoopendra.learning.inheritance;
+
+/**
+ * Created by bhokumar on 7/12/2017.
+ */
+public class SimpleSelfInheritanceTest {
+
+}
+
+class X /*extends Z*/{
+
+}
+
+class Y extends X{
+
+}
+
+class Z extends Y{
+
+}
