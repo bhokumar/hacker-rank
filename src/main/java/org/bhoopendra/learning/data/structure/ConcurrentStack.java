579acd3fa795a2d326e3c1455f6e1246f1513f56
bhoopendra <bhoopendra.akgec@gmail.com>
2017-09-19 22:21:39 +0530

    Changing values

--- src/main/java/org/bhoopendra/learning/data/structure/ConcurrentStack.java
+++ src/main/java/org/bhoopendra/learning/data/structure/ConcurrentStack.java
@@ -1,4 +1,36 @@
 package org.bhoopendra.learning.data.structure;
 
-public class ConcurrentStack {
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentStack<E> {
+    private AtomicReference<Node<E>> top = new AtomicReference<>();
+
+    public void push(final E item){
+        final Node<E> newHead = new Node<>(item);
+        Node<E> oldHead;
+        do{
+            oldHead = top.get();
+            newHead.next = oldHead;
+        }while (!top.compareAndSet(oldHead,newHead));
+    }
+    public E pop(){
+        Node<E> oldHead;;
+        Node<E> newHead;
+        do{
+            oldHead = top.get();
+            if (oldHead==null)
+                return null;
+            newHead = oldHead.next;
+        }while (!top.compareAndSet(oldHead,newHead));
+        return oldHead.item;
+    }
+
+    private static class Node<E>{
+        public E item;
+        public Node<E> next;
+
+        public Node(E item) {
+            this.item = item;
+        }
+    }
 }
