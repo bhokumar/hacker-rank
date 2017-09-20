f9c40d3681e9f5899ffb718c3c2249f208e1368d
bhokumar <bhoopendra.akgec@gmail.com>
2017-09-20 18:42:24 +0530

    FileCrawler Example

--- src/main/java/org/bhoopendra/learning/thread/blockingque/FileCrawlerIndexerClient.java
+++ src/main/java/org/bhoopendra/learning/thread/blockingque/FileCrawlerIndexerClient.java
@@ -8,7 +8,7 @@ import java.util.concurrent.BlockingQueue;
 public class FileCrawlerIndexerClient {
     public static void main(String[] args) {
         final BlockingQueue<File> fileQueue = new ArrayBlockingQueue<>(10);
-        final File[] roots = {new File("E:/code")};
+        final File[] roots = {new File("E:/code2/workspce7/hacker-rank")};
         Arrays.stream(roots).forEach((file)->new Thread(new FileCrawler(fileQueue,filteredFile->true,file)).start());
         for (int i=0;i<5;i++){
             new Thread(new Indexer(fileQueue)).start();
