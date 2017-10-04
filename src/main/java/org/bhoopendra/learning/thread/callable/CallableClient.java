cef4d1434e7b76ef801d119517cfa3351354e5c1
bhokumar <bhoopendra.akgec@gmail.com>
2017-10-04 17:53:40 +0530

    use of Callable and execution using ThreadPoolExecutor

--- src/main/java/org/bhoopendra/learning/thread/callable/CallableClient.java
+++ src/main/java/org/bhoopendra/learning/thread/callable/CallableClient.java
@@ -7,15 +7,18 @@ import java.util.logging.Logger;
 
 public class CallableClient {
     private static final Logger LOGGER = Logger.getLogger(CacheClient.class.getName());
-    public static void main(String[] args) throws InterruptedException,ExecutionException{
+    public static void main(String[] args) throws InterruptedException,ExecutionException,TimeoutException{
         final Callable<Result> resultTask = ()->{
+            Thread.sleep(1000);
             Result result = new Result();
             result.setMessage("OK");
             result.setCode(200);
             return result;
         };
+
         final ExecutorService executorService = Executors.newFixedThreadPool(2);
         final Future<Result> result = executorService.submit(resultTask);
+        LOGGER.info(result.get(1000,TimeUnit.MILLISECONDS).toString());
        Result result1 =  result.get();
        LOGGER.info(result1.getMessage());
        LOGGER.info(String.valueOf(result1.getCode()));
--- src/main/java/org/bhoopendra/learning/thread/exception/ExceptionOcurred.java
+++ src/main/java/org/bhoopendra/learning/thread/exception/ExceptionOcurred.java
@@ -2,17 +2,18 @@ package org.bhoopendra.learning.thread.exception;
 
 public class ExceptionOcurred {
 	public static void main(String[] args) throws InterruptedException {
+
 		Runnable runnable = ()->{
 			throw new RuntimeException();
 		};
+
 		Thread thread = new Thread(runnable,"thread1");
-		thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-			
-			@Override
-			public void uncaughtException(Thread t, Throwable e) {
-				System.out.println(t.getName());
-			}
+
+		thread.setUncaughtExceptionHandler((t,e)->{
+			System.out.println(t.getName());
+			System.out.println(e.getCause());
 		});
+
 		thread.start();
 		System.out.println(Thread.currentThread().getName());
 		Thread.sleep(10000);
