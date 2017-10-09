0f15f2ea6af4cb67f617be215d823f9da9d9718d
bhokumar <bhoopendra.akgec@gmail.com>
2017-10-09 19:15:16 +0530

    Adding Reflection classes

--- src/main/java/org/bhoopendra/learning/reflection/ReflectionClient.java
+++ src/main/java/org/bhoopendra/learning/reflection/ReflectionClient.java
@@ -1,6 +1,7 @@
 package org.bhoopendra.learning.reflection;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class ReflectionClient {
     public static void main(String[] args) {
@@ -14,6 +15,13 @@ public class ReflectionClient {
         for (Constructor constructor : constructors){
             constructor.setAccessible(true);
             System.out.println(constructor.getName());
+        }
+
+        System.out.println("############ Methods #########################");
+        Method[] methods = aClass.getDeclaredMethods();
+        for (Method method : methods){
+            System.out.print(method.getName()+"  ");
+            System.out.println(method.getModifiers());
         }
     }
 }
