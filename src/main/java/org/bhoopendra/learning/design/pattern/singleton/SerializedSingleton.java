3e78b8dfcc0a8fe74619fe96fa7c57d016d93eac
bhokumar <bhoopendra.akgec@gmail.com>
2017-10-09 18:40:12 +0530

    Adding Serialized classes

--- src/main/java/org/bhoopendra/learning/design/pattern/singleton/SerializedSingleton.java
+++ src/main/java/org/bhoopendra/learning/design/pattern/singleton/SerializedSingleton.java
@@ -6,17 +6,18 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class SerializedSingleton implements Serializable{
-
     private static final long serialVersionUID = -7604766932017737115L;
 
+    private final static SerializedSingleton serializedSingleton = new SerializedSingleton();
     private SerializedSingleton(){}
 
-    private static class SingletonHelper{
-        private static final SerializedSingleton instance = new SerializedSingleton();
-    }
 
     public static SerializedSingleton getInstance(){
-        return SingletonHelper.instance;
+        return serializedSingleton;
+    }
+
+    protected Object readResolve(){
+        return serializedSingleton;
     }
 }
 
@@ -26,8 +27,11 @@ class Serialization{
         SerializationUtils.serializeObject(serializedSingleton);
        Object o1 =  SerializationUtils.deSerializeObject();
        Object o2 =  SerializationUtils.deSerializeObject();
+       if (serializedSingleton.equals(o1)){
+           System.out.println("Both Objects are equal!");
+       }
        if (o1.equals(o2)){
-           System.out.println("Both Objects are equal");
+           System.out.println("Both Objects are equal!");
        }else {
            System.out.println("Object are not equal");
        }
---
+++ src/main/java/org/bhoopendra/learning/reflection/InspectionObject.java
@@ -0,0 +1,7 @@
+package org.bhoopendra.learning.reflection;
+
+public class InspectionObject {
+    private String name;
+    private int id;
+
+}
