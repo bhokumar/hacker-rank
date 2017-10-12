15d212d3934da53c87aad8f99fa244405b76fc4e
bhokumar <bhoopendra.akgec@gmail.com>
2017-10-12 17:51:57 +0530

    Ways to cover distance2

--- src/main/java/org/bhoopendra/learning/competetive/dynamic/WaysToCoverDistance2.java
+++ src/main/java/org/bhoopendra/learning/competetive/dynamic/WaysToCoverDistance2.java
@@ -4,27 +4,36 @@ package org.bhoopendra.learning.competetive.dynamic;
 // of ways to cover a distance with 1, 2 and 3 steps
 import java.io.*;
 
+// A Dynamic Programming based Java program
+// to count number of ways to cover a distance
+// with 1, 2 and 3 steps
+import java.io.*;
+
 class WaysToCoverDistance2
 {
     // Function returns count of ways to cover 'dist'
-    static int printCountRec(int dist)
+    static int printCountDP(int dist)
     {
-        // Base cases
-        if (dist<0)
-            return 0;
-        if (dist==0)
-            return 1;
+        int[] count = new int[dist+1];
 
-        // Recur for all previous 3 and add the results
-        return printCountRec(dist-1) +
-                printCountRec(dist-2) +
-                printCountRec(dist-3);
+        // Initialize base values. There is one way to
+        // cover 0 and 1 distances and two ways to
+        // cover 2 distance
+        count[0] = 1;
+        count[1] = 1;
+        count[2] = 2;
+
+        // Fill the count array in bottom up manner
+        for (int i=3; i<=dist; i++)
+            count[i] = count[i-1] + count[i-2] + count[i-3];
+
+        return count[dist];
     }
 
     // driver program
     public static void main (String[] args)
     {
         int dist = 4;
-        System.out.println(printCountRec(dist));
+        System.out.println(printCountDP(dist));
     }
 }
