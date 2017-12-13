5c255f9ed608b719483dea3a39de126d4bc58477
bhoopendra <bhoopendra.akgec@gmail.com>
2017-12-13 22:49:26 +0530

    Calling ordered method

--- src/main/java/org/bhoopendra/learning/asynchronous/AppClient.java
+++ src/main/java/org/bhoopendra/learning/asynchronous/AppClient.java
@@ -1,5 +1,19 @@
 package org.bhoopendra.learning.asynchronous;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AppClient {
-    
+
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+       // final int sum = numbers.stream().parallel().mapToInt(AppClient::transform).sum();
+        numbers.parallelStream().mapToInt(AppClient::transform).forEachOrdered(System.out::println);
+        //System.out.println(sum);
+    }
+
+    public static int transform(final int number){
+        System.out.println(Thread.currentThread()+"Number : "+number);
+        return number;
+    }
 }
