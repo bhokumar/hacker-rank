c9cedd519f87616bc84214a2e56eb7bc6fc929d3
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2019-04-04 07:57:58 +0530

    Adding HashTable client

---
+++ src/main/java/org/bhoopendra/learning/hashtables/HashTableClient.java
@@ -0,0 +1,10 @@
+package org.bhoopendra.learning.hashtables;
+
+import java.util.Hashtable;
+
+public class HashTableClient {
+    public static void main(String[] args) {
+        Hashtable<String, String> hashtable = new Hashtable<>();
+
+    }
+}
