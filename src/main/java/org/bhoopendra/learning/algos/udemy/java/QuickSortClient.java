8b5d1b5dccc94b9de65c9d8a2793aad49d475b94
bhokumar <bhoopendra.akgec@gmail.com>
2019-05-01 09:23:11 +0530

    Adding Merge and Quick sort algorithms

---
+++ src/main/java/org/bhoopendra/learning/algos/udemy/java/QuickSortClient.java
@@ -0,0 +1,44 @@
+package org.bhoopendra.learning.algos.udemy.java;
+
+public class QuickSortClient {
+    public static void main(String[] args) {
+        int intArray[] = {20, 35, -15, 7, 55, 1, -22};
+        quicksort(intArray, 0, intArray.length);
+        for (int i=0; i<intArray.length;i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void quicksort(int[] input, int start, int end) {
+
+        if(end-start<2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quicksort(input, start, pivotIndex);
+        quicksort(input, pivotIndex+1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        int pivot = input[start];
+        int i=start;
+        int j = end;
+
+        while (i<j) {
+            while (i<j && input[--j] >= pivot);
+            if(i<j) {
+                input[i] = input[j];
+            }
+
+            while (i<j && input[++i] <= pivot);
+
+            if(i<j) {
+                input[j] = input[i];
+            }
+        }
+
+        input[j] = pivot;
+        return j;
+    }
+}
